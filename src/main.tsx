31c58232af768ed618e46696b4271ef6855fba58
SpaceTimerAPI-Admin <Anthony.mchughnh@gmail.com>
2025-09-23 19:26:27 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './styles.css'
+
 import App from './App'
 import SignIn from './auth/SignIn'
 import Register from './auth/Register'
 import Home from './routes/Home'
 import Tickets from './routes/Tickets'
+import TicketDetail from './routes/TicketDetail'
 import Projects from './routes/Projects'
 import ParkMap from './routes/ParkMap'
 import Procedures from './routes/Procedures'
+import Settings from './routes/Settings'
 
 const router = createBrowserRouter([
   { path: '/', element: <SignIn /> },
@@ -20,9 +23,11 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: 'tickets', element: <Tickets /> },
+      { path: 'tickets/:id', element: <TicketDetail /> }, // detail page
       { path: 'projects', element: <Projects /> },
       { path: 'map', element: <ParkMap /> },
-      { path: 'procedures', element: <Procedures /> }
+      { path: 'procedures', element: <Procedures /> },
+      { path: 'settings', element: <Settings /> }
     ]
   }
 ])
